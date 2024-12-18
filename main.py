24d48a4e70a8aec42fab97922ffc62cfd716007a
Sriharsha Grandhi <sriharsha.grandhi26@gmail.com>
2024-12-17 21:24:09 -0500

    Adding Averages, Repetition through full list, and plotting capabilities

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Take the numbers and find minimum and maximum"""
+import matplotlib.pyplot as plt
 
 #FILE NAME
 FILE_NAME = "convolution_pairs_share.txt"
@@ -8,7 +9,8 @@ NUMBER_MULTIPLIER = 10000
 #read the file
 def read_file(stream):
     #buffer to read each line
-    total_sum = 0
+    left_total_sum = 0
+    right_total_sum = 0
     left_min = 1000
     right_min = 1000
     right_max = -1000
@@ -19,19 +21,25 @@ def read_file(stream):
     list2 = []
 #while True:
     buffer = stream.readline()
-    l1 , l2, left_min, left_max, right_min, right_max, sum1, count1 = process_buffer(buffer, right_min, right_max, left_min, left_max)
-    total_sum += sum1
-    count += count1
-    list1.extend(l1)
-    list2.extend(l2)
-    #CHECK BUFFER
-    #print(buffer)
-    return list1 , list2, right_min, right_max, left_min, left_max, total_sum, count
+    while buffer:
+        l1 , l2, left_min, left_max, right_min, right_max, sum1, sum2, count1 = process_buffer(buffer, right_min, right_max, left_min, left_max)
+        left_total_sum += sum1
+        right_total_sum += sum2
+        count += count1
+        list1.extend(l1)
+        list2.extend(l2)
+        #CHECK BUFFER
+        buffer = stream.readline()
+        #print(buffer)
+    
+    
+    return list1 , list2, right_min, right_max, left_min, left_max, left_total_sum, right_total_sum, count
 
 #process each buffer into different lists
 def process_buffer(buffer : str, right_min : float, right_max : float, left_min : float, left_max : float ):
     count = 0
-    total = 0
+    left_total = 0
+    right_total = 0
     buffer = buffer.strip()
 
     main_list = buffer.split(" ")
@@ -41,7 +49,7 @@ def process_buffer(buffer : str, right_min : float, right_max : float, left_min 
     
     for i in range(len(left_list)):
         left_list[i] = float(left_list[i])
-        total += left_list[i]
+        left_total += left_list[i]
         if(left_list[i] > left_max):
             left_max = left_list[i]
         if(left_list[i] < left_min):
@@ -50,27 +58,36 @@ def process_buffer(buffer : str, right_min : float, right_max : float, left_min 
                 
     for i in range(len(right_list)):
         right_list[i] = float(right_list[i])
-        total += right_list[i]
+        right_total += right_list[i]
         if(right_list[i] > right_max):
             right_max = right_list[i]
         if(right_list[i] < right_min):
             right_min = right_list[i]
         count += 1
         
-    return left_list , right_list, right_min, right_max, left_min, left_max, total, count
+    return left_list , right_list, left_min, left_max, right_min, right_max, left_total, right_total, count
 
 
 if(__name__ == "__main__"):
     #sum
 
     file = open(FILE_NAME , "rt")
-    l1 , l2, right_min, right_max, left_min, left_max, total_final, count = read_file(file)
-    print(f"Left List : {l1}")
-    print(f"Right List : {l2}")
+    l1 , l2, right_min, right_max, left_min, left_max, left_total_final, right_total_final, count = read_file(file)
+    left_avg = float(left_total_final)/ float(count/2)
+    right_avg = float(right_total_final) / float(count/2)
+    
+   # print(f"Left List : {l1}")
+   # print(f"Right List : {l2}")
     print(f"Right Max : {right_max}")
     print(f"Right Min : {right_min}")
     print(f"Left Max : {left_max}")
     print(f"Left Min : {left_min}")
-    print(f"Total : {total_final}")
+    print(f"Left Total : {left_total_final}")
+    print(f"Right Total : {right_total_final}")
+    print(f"Left Average : {left_avg}")
+    print(f"Right Average : {right_avg}")   
     print(f"Count : {count}")
     
+    plt.boxplot(l1)
+    plt.boxplot(l2)
+    plt.show()
