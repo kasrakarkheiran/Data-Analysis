59121a5898386a961e19d31b913c2520ae3d26ed
Kasra <golden.kasra@gmail.com>
2025-02-27 16:13:07 -0500

    pp

--- FPGA_TEST/Bittream.py
+++ FPGA_TEST/Bittream.py
@@ -1,11 +1,11 @@
 import numpy as np
 import struct
 RANGE = 128000
-value = 3060
+value = 0
 DATA = packed_data = struct.pack("<h", value)
 
 with open('FPGA_TEST/bit.bin', 'wb') as f:
-    for i in range (0,64000):
+    for i in range (0,700000):
         f.write(DATA)
     f.close()   
         
