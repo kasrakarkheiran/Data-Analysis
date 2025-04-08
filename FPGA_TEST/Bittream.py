41bb3bee94b13183cbb29a3def6fb2acfd58964a
Kasra <golden.kasra@gmail.com>
2025-04-08 13:04:24 -0400

    Added a script to plot image data as a wave and plot it. We also save 128KB of data into the wave_data.bin

--- FPGA_TEST/Bittream.py
+++ FPGA_TEST/Bittream.py
@@ -1,11 +1,29 @@
+from PIL import Image
 import numpy as np
-import struct
-RANGE = 128000
-value = 1
-DATA = packed_data = struct.pack("<h", value)
+import matplotlib.pyplot as plt
 
-with open('FPGA_TEST/bit.bin', 'wb') as f:
-    for i in range (0,65536):
-        f.write(DATA)
-    f.close()   
-        
+# Load and convert to RGB
+image = Image.open("D:/DataAnalysis/Data-Analysis/FPGA_TEST/image.jpeg").convert("RGB")
+pixels = np.array(image)
+
+# Flatten pixels to (num_pixels, 3)
+flat_pixels = pixels.reshape(-1, 3)
+
+# Combine RGB into a single intensity per pixel (grayscale)
+intensity = np.mean(flat_pixels, axis=1)  # or use weighted formula for luminance
+#write to binary file
+MAX_BYTES = 128 * 1024  # 128 KB
+data_to_bytes = intensity.tobytes()[:MAX_BYTES]
+with open("D:/DataAnalysis/Data-Analysis/FPGA_TEST/wave_data.bin", "wb") as f:
+    f.write(data_to_bytes)
+print("Wave saved to 'wave_data.bin'")
+
+# Plot the intensity wave
+"""
+plt.figure(figsize=(15, 5))
+plt.plot(intensity, color='gray')
+plt.title('Combined RGB Intensity Wave')
+plt.xlabel('Pixel Index')
+plt.ylabel('Intensity (0-255)')
+plt.tight_layout()
+plt.show()"""
