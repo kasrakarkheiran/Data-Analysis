ce947db9bb81a4278b327186fcb5e9d21a33f47f
Kasra <golden.kasra@gmail.com>
2025-04-10 13:31:25 -0400

    Created a test bin file to check captures on ADC

--- FPGA_TEST/Bittream.py
+++ FPGA_TEST/Bittream.py
@@ -1,29 +1,14 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import struct
 
-# Load and convert to RGB
-image = Image.open("D:/DataAnalysis/Data-Analysis/FPGA_TEST/image.jpeg").convert("RGB")
-pixels = np.array(image)
-
-# Flatten pixels to (num_pixels, 3)
-flat_pixels = pixels.reshape(-1, 3)
-
-# Combine RGB into a single intensity per pixel (grayscale)
-intensity = np.mean(flat_pixels, axis=1)  # or use weighted formula for luminance
-#write to binary file
-MAX_BYTES = 128 * 1024  # 128 KB
-data_to_bytes = intensity.tobytes()[:MAX_BYTES]
-with open("D:/DataAnalysis/Data-Analysis/FPGA_TEST/wave_data.bin", "wb") as f:
-    f.write(data_to_bytes)
-print("Wave saved to 'wave_data.bin'")
-
-# Plot the intensity wave
-"""
-plt.figure(figsize=(15, 5))
-plt.plot(intensity, color='gray')
-plt.title('Combined RGB Intensity Wave')
-plt.xlabel('Pixel Index')
-plt.ylabel('Intensity (0-255)')
-plt.tight_layout()
-plt.show()"""
+#load test numbers
+numbers = []
+for i in range(0, 65536):
+    numbers.append(i%255)
+# Open the file for writing in binary mode
+with open('D:/DataAnalysis/Data-Analysis/FPGA_TEST/wave_data.bin', 'wb') as f:
+    for num in numbers:
+        # '<h' means little-endian ('<') 16-bit signed integer ('h')
+        f.write(struct.pack('<h', num))
